b0f654c2707a09e9fb7d77ccdb4f2bd865444cf7
ToobaBukhari <toobafatimabukhari@gmail.com>
2023-10-20 14:14:44 +0500

    change

--- Funtions/fn.js
+++ Funtions/fn.js
@@ -2,6 +2,12 @@
 function Greet(para, para2) {
     return para + para2;
 }
-console.log(Greet("Hello World!", +56));
-
-
+console.log(`This is a Template Literals Greet: ${Greet("Hello World!", +56)}`);
+function people(para, para2) {
+    return para + para2;
+}
+console.log(`Hi people World like to learn some funtion today! ${people("Function:", "Arrowfunctions")}`);
+function hello(para, para2) {
+    return para - para2;
+}
+console.log(`I subtracts the para is 4 and para2 is 5 & Answer is: ${hello(4, 5)}`);
--- Funtions/fn.ts
+++ Funtions/fn.ts
@@ -1,11 +1,22 @@
 
-function Greet(para:string,para2:number):any{
-    return para+para2
+function Greet(para:string  ,  para2:number):any{
+    return para +  para2
 }
 
-console.log(Greet("Hello World!",+56));
+console.log(`This is a Template Literals Greet: ${Greet("Hello World!" , + 56 )}`);
 
 
+function people(para:string,para2:string){
+    return para+ para2;
+}
+
+console.log(`Hi people World like to learn some funtion today! ${people("Function:" , "Arrowfunctions")}`);
+
+function hello(para:number,para2:number):any{
+    return para-para2;
+}
+
+console.log(`I subtracts the para is 4 and para2 is 5 & Answer is: ${hello(4,5)}`);
 
 
 
